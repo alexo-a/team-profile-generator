f07b956a17a2e752dc894f9979b25c8a4baa4131
alexo-a <a.ostrowski.11@gmail.com>
2020-08-02 16:41:11 -0500

    employee methods tested and successful

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,9 +1,17 @@
 const Employee = require("../lib/Employee.js");
 //const { exportAllDeclaration } = require("@babel/types");
 
-test("Checks to see if creating an employee works", () => {
+test("Checks employee properties", () => {
     const employee = new Employee("Dave", "22", "dave@company.com")
     expect(employee.name).toEqual("Dave");
     expect(employee.id).toEqual("22");
     expect(employee.email).toEqual("dave@company.com");
+})
+
+test("checks employee methods .getName, .getId, .getEmail, and .getRole", ()=> {
+    const employee = new Employee("Dave", "22", "dave@company.com")
+    expect(employee.getName()).toEqual("Dave");
+    expect(employee.getId()).toEqual("22");
+    expect(employee.getEmail()).toEqual("dave@company.com");
+    expect(employee.getRole()).toEqual("Employee");
 })
